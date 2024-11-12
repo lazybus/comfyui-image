b6d63638d0aaa0fdf584b4493ec2aeb451bed793
Hank Hill <cmajor7th.chord@gmail.com>
2024-11-12 07:09:50 -0500

    testing provisioning script with python packages that are failing for nodes instantID and layerstyle

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -8,17 +8,64 @@
 
 PYTHON_PACKAGES=(
     #"opencv-python==4.7.0.72"
+	"insightface"
+	"numpy<2.0",
+	"Pillow",
+	"torch",
+	"matplotlib",
+	"Scipy",
+	"scikit-image",
+	"scikit-learn",
+	"opencv-contrib-python",
+	"pymatting",
+	"segment-anything",
+	"timm",
+	"addict",
+	"yapf",
+	"colour-science",
+	"wget",
+	"mediapipe",
+	"loguru",
+	"typer-config",
+	"fastapi",
+	"rich",
+	"google-generativeai",
+	"diffusers",
+	"omegaconf",
+	"tqdm",
+	"kornia",
+	"ultralytics>=8.2.0",
+	"blend-modes",
+	"blind-watermark",
+	"qrcode",
+	"pyzbar",
+	"transparent-background",
+	"huggingface-hub>=0.23.4",
+	"accelerate>=0.26.0",
+	"onnxruntime",
+	"torchscale",
+	"wandb",
+	"psd-tools",
+	"hydra-core",
+	"protobuf>=3.20.3",
+	"inference-cli>=0.13.0",
+	"inference-gpu[yolo-world]>=0.13.0",
+	"bitsandbytes>=0.41.1",
+	"transformers>=4.43.2",
+	"peft>=0.12.0",
+	"iopath"
+	"onnxruntime-gpu; sys_platform != 'darwin' and platform_machine == 'x86_64'"
 )
 
 NODES=(
-    "https://github.com/ltdrdata/ComfyUI-Manager"
-    "https://github.com/WASasquatch/was-node-suite-comfyui"
-    "https://github.com/sipherxyz/comfyui-art-venture"
-    "https://github.com/kijai/ComfyUI-KJNodes"    
-    "https://github.com/storyicon/comfyui_segment_anything"
-    "https://github.com/chflame163/ComfyUI_LayerStyle"
-    "https://github.com/TheMistoAI/ComfyUI-Anyline"
-    "https://github.com/42lux/ComfyUI-safety-checker"
+    #"https://github.com/ltdrdata/ComfyUI-Manager"
+    #"https://github.com/WASasquatch/was-node-suite-comfyui"
+    #"https://github.com/sipherxyz/comfyui-art-venture"
+    #"https://github.com/kijai/ComfyUI-KJNodes"    
+    #"https://github.com/storyicon/comfyui_segment_anything"
+    #"https://github.com/chflame163/ComfyUI_LayerStyle"
+    #"https://github.com/TheMistoAI/ComfyUI-Anyline"
+    #"https://github.com/42lux/ComfyUI-safety-checker"
 )
 
 CHECKPOINT_MODELS=(
@@ -26,9 +73,9 @@ CHECKPOINT_MODELS=(
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
-    "https://huggingface.co/tobstudios/n1/resolve/main/model.safetensors",
-    "https://huggingface.co/moiu2998/mymo/resolve/3c3093fa083909be34a10714c93874ce5c9dabc4/realisticVisionV60B1_v51VAE.safetensors"
-    "https://huggingface.co/RunDiffusion/Juggernaut-X-v10/resolve/main/Juggernaut-X-RunDiffusion-NSFW.safetensors"
+    #"https://huggingface.co/tobstudios/n1/resolve/main/model.safetensors",
+    #"https://huggingface.co/moiu2998/mymo/resolve/3c3093fa083909be34a10714c93874ce5c9dabc4/realisticVisionV60B1_v51VAE.safetensors"
+    #"https://huggingface.co/RunDiffusion/Juggernaut-X-v10/resolve/main/Juggernaut-X-RunDiffusion-NSFW.safetensors"
 )
 
 LORA_MODELS=(
@@ -48,31 +95,31 @@ ESRGAN_MODELS=(
 )
 
 ICLIGHT=(
-    "https://huggingface.co/lllyasviel/ic-light/resolve/main/iclight_sd15_fbc.safetensors"
-    "https://huggingface.co/lllyasviel/ic-light/resolve/main/iclight_sd15_fc.safetensors"
+    #"https://huggingface.co/lllyasviel/ic-light/resolve/main/iclight_sd15_fbc.safetensors"
+    #"https://huggingface.co/lllyasviel/ic-light/resolve/main/iclight_sd15_fc.safetensors"
     #"https://huggingface.co/ai-forever/Real-ESRGAN/resolve/main/RealESRGAN_x4.pth"
     #"https://huggingface.co/FacehugmanIII/4x_foolhardy_Remacri/resolve/main/4x_foolhardy_Remacri.pth"
     #"https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x_NMKD-Siax_200k.pth"
 )
 
 CLIP_VISION=(
-    "https://huggingface.co/h94/IP-Adapter/resolve/main/models/image_encoder/model.safetensors" #clipH14.safetensors
+    #"https://huggingface.co/h94/IP-Adapter/resolve/main/models/image_encoder/model.safetensors" #clipH14.safetensors
 )
 
 IPADAPTER=(
-    "https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors"
+    #"https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors"
 )
 
 INPAINT=(
-    "https://huggingface.co/Acly/MAT/resolve/main/MAT_Places512_G_fp16.safetensors"
+    #"https://huggingface.co/Acly/MAT/resolve/main/MAT_Places512_G_fp16.safetensors"
 )
 
 
 
 CONTROLNET_MODELS=(
-    "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_canny_full.safetensors"
-    "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_depth_full.safetensors"
-    "https://huggingface.co/TheMistoAI/MistoLine/resolve/main/mistoLine_fp16.safetensors"
+    #"https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_canny_full.safetensors"
+    #"https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_depth_full.safetensors"
+    #"https://huggingface.co/TheMistoAI/MistoLine/resolve/main/mistoLine_fp16.safetensors"
     #"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_canny-fp16.safetensors"
     #"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_depth-fp16.safetensors"
     #"https://huggingface.co/kohya-ss/ControlNet-diff-modules/resolve/main/diff_control_sd15_depth_fp16.safetensors"
@@ -116,9 +163,6 @@ function provisioning_start() {
     provisioning_get_models \
         "${WORKSPACE}/storage/stable_diffusion/models/esrgan" \
         "${ESRGAN_MODELS[@]}"
-    provisioning_get_models \
-        "${WORKSPACE}/storage/stable_diffusion/models/unet/IC-Light" \
-        "${ICLIGHT[@]}"
     provisioning_get_models \
         "${WORKSPACE}/storage/stable_diffusion/models/clip_vision" \
         "${CLIP_VISION[@]}"
